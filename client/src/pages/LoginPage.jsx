b2d27bfb8611d2f5489a50ba7077762286edbf2f
Tilakrajrawat <tilakrajrawat1234@gmail.com>
2025-06-29 22:20:56 +0530

    homePage complete

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import assets from "../assets/assets"
 
 const LoginPage = () => {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
-}
+  const [currState, setCurrState] = useState("Sign Up");
 
-export default LoginPage
+  return (
+    <div className="min-h-screen bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
+      {/* Background image for left side */}
+      <img src={assets.logo_pic} alt="" className="w-[min(30vw,250px)]" />
+      {/* Login form container */}
+      <form
+        action=""
+        className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg"
+      >
+        <h2 className="font-medium text-2xl flex justify-between items-center">
+          {currState}
+          <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />
+        </h2>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
